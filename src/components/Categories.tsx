827fd8d3216826c5147e327ca4988984cd89ba74
geuna <geuna.bae@im-fine.co.kr>
2023-03-08 01:38:10 +0900

    [feat] CategoryPage 레이아웃 및 기능 구현

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import {
+  CategorySelectType,
+  CATEGORY_SELECT_LIST,
+  ExpendType,
+} from "../config";
+import { useAppSelector } from "../context/redux";
+import sortingData from "../utils/sortingData";
+import DetailPreview from "./DetailPreview";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  const [target, setTarget] = useState<CategorySelectType>("All");
+  const [targetDataArr, setTargetDataArr] = useState<ExpendType[]>([]);
+  const [targetData, setTargetData] = useState<ExpendType>();
+
+  const data = useAppSelector((state) => state.expend);
+
+  useEffect(() => {
+    if (target === "All") setTargetDataArr(data);
+    else {
+      const arr = data.filter((val) => val.category === target);
+      setTargetDataArr(arr);
+    }
+  }, [data, target]);
+
+  const handleTargetData = (target: ExpendType) => setTargetData(target);
+
+  return (
+    <StCategories>
+      <StHeader>
+        <ul>
+          {CATEGORY_SELECT_LIST.map((val) => {
+            return (
+              <li onClick={() => setTarget(val)} key={val}>
+                {val}
+              </li>
+            );
+          })}
+        </ul>
+      </StHeader>
+      <StBody>
+        {sortingData(targetDataArr, "date", false).map((val) => {
+          return (
+            <React.Fragment key={val.id}>
+              <DetailPreview
+                handleTargetData={handleTargetData}
+                targetData={targetData}
+                val={val}
+              />
+            </React.Fragment>
+          );
+        })}
+      </StBody>
+    </StCategories>
+  );
 };
 
 export default Categories;
+
+const StCategories = styled.div``;
+
+const StHeader = styled.div``;
+
+const StBody = styled.div``;
