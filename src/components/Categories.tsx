2829d5d248563bcd3c7b61db2bc478b41dda1fb1
geuna <geuna0204@gmail.com>
2023-03-10 07:16:51 +0900

    [design] CategoryPage 스타일링

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import {
   CategorySelectType,
+  CategoryType,
   CATEGORY_SELECT_LIST,
   ExpendType,
 } from "../config";
 import { useAppSelector } from "../context/redux";
+import { calcRem, theme } from "../styles/theme";
+import { priceConverter } from "../utils/priceConverter";
 import { sortingData } from "../utils/sortingData";
+import CategoryIcon from "./CategoryIcon";
 import DetailPreview from "./DetailPreview";
 
 const Categories = () => {
@@ -29,28 +33,45 @@ const Categories = () => {
   return (
     <StCategories>
       <StHeader>
-        <ul>
+        <StTitle>
+          Category Total{" "}
+          <span>
+            {
+              priceConverter(
+                targetDataArr
+                  .map((val) => Number(val.price))
+                  .reduce((acc, cur) => acc + cur, 0)
+                  .toString()
+              ).previewPrice
+            }{" "}
+            ₩
+          </span>
+        </StTitle>
+        <StCategoryList>
           {CATEGORY_SELECT_LIST.map((val) => {
             return (
               <li onClick={() => setTarget(val)} key={val}>
-                {val}
+                <CategoryIcon target={val as CategoryType} />
+                <span>{val}</span>
               </li>
             );
           })}
-        </ul>
+        </StCategoryList>
       </StHeader>
       <StBody>
-        {sortingData(targetDataArr, "date", false).map((val) => {
-          return (
-            <React.Fragment key={val.id}>
-              <DetailPreview
-                handleTargetData={handleTargetData}
-                targetData={targetData}
-                val={val}
-              />
-            </React.Fragment>
-          );
-        })}
+        <StExpendList>
+          {sortingData(targetDataArr, "title", true).map((val) => {
+            return (
+              <React.Fragment key={val.id}>
+                <DetailPreview
+                  handleTargetData={handleTargetData}
+                  targetData={targetData}
+                  val={val}
+                />
+              </React.Fragment>
+            );
+          })}
+        </StExpendList>
       </StBody>
     </StCategories>
   );
@@ -58,8 +79,62 @@ const Categories = () => {
 
 export default Categories;
 
-const StCategories = styled.div``;
+const StCategories = styled.div`
+  padding: ${calcRem(10)} ${calcRem(20)} ${calcRem(20)} ${calcRem(20)};
+  display: flex;
+  flex-direction: column;
+  gap: ${calcRem(50)};
+`;
 
 const StHeader = styled.div``;
 
+const StTitle = styled.h2`
+  color: ${theme.blue3};
+  font-family: "Rubik";
+  font-size: ${calcRem(20)};
+  font-weight: 700;
+
+  span {
+    margin-left: ${calcRem(4)};
+    color: ${theme.blue3};
+    font-family: "Rubik";
+    font-size: ${calcRem(20)};
+    font-weight: 700;
+  }
+`;
+
+const StCategoryList = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: ${calcRem(14)};
+  margin-top: ${calcRem(14)};
+  padding: ${calcRem(12)} ${calcRem(10)};
+  background-color: ${theme.gray1};
+  overflow-x: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+
+  li {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: ${calcRem(4)};
+
+    span {
+      color: ${theme.blue3};
+      font-size: ${calcRem(10)};
+    }
+  }
+`;
+
 const StBody = styled.div``;
+
+const StExpendList = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: ${calcRem(12)};
+`;
