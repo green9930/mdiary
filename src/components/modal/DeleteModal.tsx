be539de27e5d8ba7b9dda01d071a6530ed02950b
geuna <geuna0204@gmail.com>
2023-03-11 02:20:03 +0900

    [design] DeleteModal 스타일링

--- src/components/modal/DeleteModal.tsx
+++ src/components/modal/DeleteModal.tsx
@@ -1,10 +1,11 @@
 import { deleteDoc, doc } from "firebase/firestore";
-import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { ExpendType } from "../../config";
 import { deleteExpend } from "../../context/modules/expendSlice";
 import { dbService } from "../../firebase";
+import { calcRem } from "../../styles/theme";
+import Button from "../elements/Button";
 
 interface ISelectDeleteModal {
   handleClose: () => void;
@@ -19,8 +20,6 @@ const DeleteModal = ({
 }: ISelectDeleteModal) => {
   const dispatch = useDispatch();
 
-  console.log(target);
-
   const handleDelete = async () => {
     await deleteDoc(doc(dbService, "expend", target.id as string));
     dispatch(deleteExpend(target));
@@ -31,14 +30,45 @@ const DeleteModal = ({
   return (
     <StDeleteModal>
       <p>해당 내용을 삭제하시겠습니까?</p>
-      <div>
-        <button onClick={handleDelete}>OK</button>
-        <button onClick={handleClose}>취소</button>
-      </div>
+      <StBtnWrapper>
+        <Button btnTheme="beige3" btnSize="small1" onClick={handleDelete}>
+          OK
+        </Button>
+        <Button btnTheme="blue1" btnSize="small1" onClick={handleClose}>
+          취소
+        </Button>
+      </StBtnWrapper>
     </StDeleteModal>
   );
 };
 
 export default DeleteModal;
 
-const StDeleteModal = styled.div``;
+const StDeleteModal = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  p {
+    width: 100%;
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
+    font-size: ${calcRem(16)};
+    font-weight: 500;
+  }
+`;
+
+const StBtnWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: ${calcRem(26)};
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+`;
