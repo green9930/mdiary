22a3081aafb40476c3125bfb6013865706b5654f
geuna <geuna0204@gmail.com>
2023-03-13 04:53:13 +0900

    [feat] 프로젝트 PWA 적용

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import App from "./App";
 import store from "./context/redux";
 import GlobalStyle from "./styles/GlobalStyle";
@@ -33,4 +34,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorkerRegistration.register();
 reportWebVitals();
