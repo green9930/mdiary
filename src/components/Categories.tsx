52026fbd34a045e3fff5be26b1579e6c0b86fd7a
geuna <geuna0204@gmail.com>
2023-09-04 02:32:03 +0900

    [feat] Category 페이지 날짜별 필터링 기능 추가

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,86 +1,146 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+
+import ModalLayout from "./layout/ModalLayout";
+import DateSelectModal from "./modal/DateSelectModal";
 import CategoryIcon from "./CategoryIcon";
 import DetailPreview from "./DetailPreview";
 import { useAppSelector } from "../context/redux";
-import { calcRem, theme } from "../styles/theme";
 import { priceConverter } from "../utils/priceConverter";
 import { sortingData } from "../utils/sortingData";
+import { dateToStr } from "../utils/dateConverter";
 import {
   CategorySelectType,
   CategoryType,
   CATEGORY_SELECT_LIST,
   ExpendType,
 } from "../config";
+import { MOBILE_MAX_W, WINDOW_W, calcRem, theme } from "../styles/theme";
 
 const Categories = () => {
-  const [target, setTarget] = useState<CategorySelectType>("All");
+  const [loading, setLoading] = useState(true);
+  const [targetCategory, setTargetCategory] =
+    useState<CategorySelectType>("All");
+  const [targetDate, setTargetDate] = useState<Date>(new Date());
+  const [year, setYear] = useState(0);
+  const [month, setMonth] = useState(0);
   const [targetDataArr, setTargetDataArr] = useState<ExpendType[]>([]);
   const [targetData, setTargetData] = useState<ExpendType>();
+  const [openDateModal, setOpenDateModal] = useState(false);
 
   const data = useAppSelector((state) => state.expend);
 
   useEffect(() => {
-    if (target === "All") setTargetDataArr(data);
-    else {
-      const arr = data.filter((val) => val.category === target);
-      setTargetDataArr(arr);
-    }
-  }, [data, target]);
+    const currentDate = new Date();
+    const yy = currentDate.getFullYear();
+    const mm = currentDate.getMonth();
+
+    setTargetDate(currentDate);
+    setYear(yy);
+    setMonth(mm);
+
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (!loading) handleDataArr();
+  }, [data, targetCategory, targetDate]);
+
+  useEffect(() => {
+    const yy = targetDate.getFullYear();
+    const mm = targetDate.getMonth();
+
+    setYear(yy);
+    setMonth(mm);
+  }, [targetDate]);
+
+  const handleDataArr = () => {
+    const mData = data.filter((val) => {
+      const yy = targetDate.getFullYear();
+      const mm = targetDate.getMonth();
+      const yymm = `${yy}.${dateToStr(mm + 1)}`;
+      const valDate = `${val.date.split("-")[0]}.${val.date.split("-")[1]}`;
+      return targetCategory === "All"
+        ? valDate === yymm
+        : valDate === yymm && val.category === targetCategory;
+    });
+    setTargetDataArr(mData);
+  };
 
   const handleTargetData = (target: ExpendType) => setTargetData(target);
 
   return (
-    <StCategories>
-      <StHeader>
-        <StTitle>
-          Category Total{" "}
-          <span>
-            {
-              priceConverter(
-                targetDataArr
-                  .map((val) => Number(val.price))
-                  .reduce((acc, cur) => acc + cur, 0)
-                  .toString()
-              ).previewPrice
-            }{" "}
-            ₩
-          </span>
-        </StTitle>
-        <StCategoryList>
-          {CATEGORY_SELECT_LIST.map((val) => {
-            return (
-              <li onClick={() => setTarget(val)} key={val}>
-                <CategoryIcon target={val as CategoryType} />
-                <span>{val}</span>
-              </li>
-            );
-          })}
-        </StCategoryList>
-      </StHeader>
-      <StBody>
-        <StExpendList>
-          {sortingData(targetDataArr, "date", false).map((val, idx, arr) => {
-            const i = idx === 0 ? 0 : idx - 1;
-            const valYearMonth = val.date.slice(0, 7);
-            return (
-              <React.Fragment key={val.id}>
-                {valYearMonth !== arr[i].date.slice(0, 7) || idx === 0 ? (
-                  <span className="month-section">
-                    {valYearMonth.replace("-", ".")}
-                  </span>
-                ) : null}
-                <DetailPreview
-                  handleTargetData={handleTargetData}
-                  targetData={targetData}
-                  val={val}
-                />
-              </React.Fragment>
-            );
-          })}
-        </StExpendList>
-      </StBody>
-    </StCategories>
+    <>
+      {!loading ? (
+        <StCategories>
+          <StHeader>
+            <StTitle>
+              {`${year}.${dateToStr(month + 1)} Total `}
+              <span>
+                {
+                  priceConverter(
+                    targetDataArr
+                      .map((val) => Number(val.price))
+                      .reduce((acc, cur) => acc + cur, 0)
+                      .toString()
+                  ).previewPrice
+                }{" "}
+                ₩
+              </span>
+            </StTitle>
+            <StCategoryList>
+              {CATEGORY_SELECT_LIST.map((val) => {
+                return (
+                  <li onClick={() => setTargetCategory(val)} key={val}>
+                    <CategoryIcon target={val as CategoryType} />
+                    <StCategoryText isSelected={val === targetCategory}>
+                      {val}
+                    </StCategoryText>
+                  </li>
+                );
+              })}
+            </StCategoryList>
+            <StMonth>
+              <button onClick={() => setOpenDateModal(true)}>
+                <span>기간 선택</span>
+              </button>
+            </StMonth>
+          </StHeader>
+          <StBody>
+            <StExpendList>
+              {sortingData(targetDataArr, "date", false).map(
+                (val, idx, arr) => (
+                  <DetailPreview
+                    handleTargetData={handleTargetData}
+                    targetData={targetData}
+                    val={val}
+                  />
+                )
+              )}
+            </StExpendList>
+          </StBody>
+          {openDateModal ? (
+            <ModalLayout
+              width={WINDOW_W < MOBILE_MAX_W ? "320px" : "360px"}
+              height="auto"
+            >
+              <DateSelectModal
+                type="month"
+                year={year}
+                month={month}
+                date={1}
+                setTargetDate={setTargetDate}
+                handleModal={setOpenDateModal}
+              />
+            </ModalLayout>
+          ) : (
+            <></>
+          )}
+        </StCategories>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
@@ -91,6 +151,7 @@ const StCategories = styled.div`
   flex-direction: column;
   gap: ${calcRem(32)};
   padding: ${calcRem(20)};
+  padding-top: ${calcRem(120)};
 `;
 
 const StHeader = styled.div``;
@@ -124,10 +185,34 @@ const StCategoryList = styled.ul`
     flex-direction: column;
     align-items: center;
     gap: ${calcRem(4)};
+  }
+`;
+
+const StCategoryText = styled.span<{ isSelected: boolean }>`
+  color: ${({ isSelected }) => (isSelected ? theme.red2 : theme.blue3)};
+  font-size: ${calcRem(10)};
+  font-weight: ${({ isSelected }) => (isSelected ? 700 : 500)};
+`;
+
+const StMonth = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: ${calcRem(4)};
+
+  button {
+    display: flex;
+    align-items: center;
+    gap: ${calcRem(4)};
+    padding: ${calcRem(6)};
+    border: none;
+    background-color: transparent;
 
     span {
-      color: ${theme.blue3};
-      font-size: ${calcRem(10)};
+      font-size: ${calcRem(14)};
+      font-weight: 500;
+      color: ${theme.green1};
     }
   }
 `;
